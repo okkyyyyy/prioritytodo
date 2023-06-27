79ac91fab18428339805c547a8be3e44f99daa91
finanazwa <137764298+finanazwa@users.noreply.github.com>
2023-06-27 14:21:41 +0800

    Update priority_todo.py

--- priority_todo.py
+++ priority_todo.py
@@ -20,20 +20,6 @@ st.title("To-Do List")
 # Get the number of tasks from the user
 num_tasks = st.number_input("Enter the number of tasks:", min_value=1, step=1)
 
-# Input our data
-with st.form(key='submit_data'):
-    date = st.date_input("Date Input") # Input the date
-    weight = st.weight_input("Weight", 0, 200) # Input the weight
-    submitted = st.form_submit_button("Submit")
-if submitted : # if button clicked
-        if date in data['date'].values : # if the date specified already exist in our data
-            data.loc[data['date']==date,'weight'] = weight # then we replace the weight
-        else :  # if the date specified does not exist in our data
-            new_record = pd.DataFrame({'weight':[weight],'date':[date]}) # create a new record
-            data= pd.concat([data,new_record],axis=0) # combine it with existing data
-        
-        data['date'] = pd.to_datetime(data['date']).dt.normalize() # dt.normalize to remove time component since we only need the date 
-        data.to_csv('historical_weight.csv',index=False) # save changes back to the file
 
 # Prompt the user to input task details
 for i in range(1, num_tasks + 1):
