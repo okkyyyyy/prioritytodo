46954644626e27b0b87748a8e130056f40a9683a
finanazwa <137764298+finanazwa@users.noreply.github.com>
2023-06-27 14:27:12 +0800

    Update priority_todo.py

--- priority_todo.py
+++ priority_todo.py
@@ -19,7 +19,6 @@ st.title("To-Do List")
 
 # Get the number of tasks from the user
 num_tasks = st.number_input("Enter the number of tasks:", min_value=1, step=1)
-submitted = st.form_submit_button("Submit")
         
 
 # Prompt the user to input task details
